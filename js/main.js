8c77bdf18288176e9a5075c132667f8c9a5b2857
Dave-314land <dave.l.piland@gmail.com>
2021-06-02 07:55:55 -0400

    added checkbox functionality to set 'Start with' phrase at the zero index of the array

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 let loremIpsumDictionary = [
   `Abdullah Express`,
+  `A.J. Villain`,
   `Andy`,
   `Andy's Almost Upset of the Week`,
+  `Announcing...Jared Garf`,
   `Appstanding`,
   `Arizona`,
+  `Awesome Eckler`,
   `Ballers`,
+  `Big Herb`,
+  `Big Montana`,
   `Blake the Snake`,
   `Bonjuour`,
   `Bonus`,
   `Booty Scootin Cam Newtown`,
   `Borgogen`,
   `Borland`,
+  `Brandon Aiyuken`,
   `Brooks`,
+  `Bruce Wayne Gallman`,
   `Cardinals`,
+  `Carry on Johnson`,
+  `Chris Goblin`,
+  `Darnold Schwarzenegger`,
+  `Derrick Yeti`,
+  `Darren Wallerus`,
   `David Johnson woo-hoo-hoo`,
+  `David Mopportunity`,
+  `Devin 'S' Scrumptious`,
+  `Dirty cake`,
   `Don't hear what I'm not saying`,
   `Double Stuff Oreos`,
   `Dynasty Download`,
@@ -26,16 +41,26 @@ let loremIpsumDictionary = [
   `Footballers`,
   `Footclan`,
   `Footclan Assemble`,
-  `Gas Man`,
+  `Get to the touchdown`,
+  `He is infinite, he is all`,
   `He was drafted to be great`,
   `Hey`,
+  `Hey, don't forget about me, Mark Waltonburg`,
+  `Hey, it's me, Dave`,
   `Hockstrap`,
+  `Hockules`,
+  `Hot Locket`,
   `I apologize for nothing`,
+  `I've believe you've met my fitness consigliere, Sony Michele`,
   `It's football time`,
   `Jason`,
+  `Jared Cuuuooke`,
+  `Ja Rule - murrrder`,
   `Jay Grizz`,
+  `Jimmy Grahmpa`,
   `Josh Stallion (excellent)`,
   `Judge Giamatti`,
+  `Kenny Bills - Money money money money money`,
   `Kenny G (so smooth)`,
   `Lizard King`,
   `Mail bag`,
@@ -44,27 +69,42 @@ let loremIpsumDictionary = [
   `Manilla Ice`,
   `Matty Ice`,
   `Matty Snapback`,
+  `MEGALADON`,
   `Mike`,
   `Mock-a lock-a ding dong`,
   `Muscle Hamster`,
+  `Mr. B-hole`,
+  `Mr. Irresponsible`,
   `My Name is Jeff`,
   `News and Notes from around the league`,
   `New York City`,
+  `Now you're a man, a man, man, man`,
   `Oh the Garbage Man can`,
-  `Oreos`,
+  `One more big score`,
   `Pee River`,
   `Peter Piper Pizza PPR Pick of the Week`,
   `Phoenix`,
   `Pooped in his big boy pants`,
+  `Red Dead Dixon`,
   `Reviewasaurus Rex`,
+  `Rule 86`,
   `Sammy Biscuits`,
+  `Say hi to your mother for me`,
+  `Send in the Carr`,
   `Smash Jackson`,
   `Splash`,
   `TFFB`,
+  `The Gas Man`,
   `The Fantasy Footballers`,
+  `The Flying V`,
+  `The Infinity Stone`,
   `This one comes in from`,
+  `The moose is loose`,
   `Tony Meatballs`,
   `Trash Man`,
+  `Turok, the dinosaur hunter, Cohen`,
+  `T.Y. Houston`,
+  `Unter Enry`,
   `Wanna play a game`,
   `Waterbet`,
   `Weeeeeelcome In`,
@@ -74,9 +114,10 @@ let loremIpsumDictionary = [
 ];
 
 // Define variables
+const ballersArrayLength = loremIpsumDictionary.length;
 const form = document.querySelector(".lorem-form");
 const result = document.querySelector(".lorem-text");
-const ballersArrayLength = loremIpsumDictionary.length;
+const checkbox = document.querySelector("#start-with");
 const paragraphsRange = document.getElementById("numberOfParagraphsRange");
 const paragraphsNumber = document.getElementById("numberOfParagraphsNumber");
 
@@ -103,51 +144,72 @@ function getRandomNum(ballersArrayLength) {
   return Math.floor(Math.random() * ballersArrayLength);
 }
 
+function isChecked() {
+  if (checkbox.checked) {
+    return true;
+  }
+}
+
+//function startWithIpsum(e) {}
+
 function getUserLoremIpsum(e) {
   let tempArray = [];
   // Converting the paragraphsNumber.value variable into a number for later comparison
   const numParagraphs = parseInt(paragraphsNumber.value);
   // Setting the number of indecies to pull based on the number of paragraphs chosen by the user
   if (numParagraphs === 1) {
-    textLength = 100;
+    textLength = 60;
   } else if (numParagraphs === 2) {
-    textLength = 200;
+    textLength = 120;
   } else if (numParagraphs === 3) {
-    textLength = 300;
+    textLength = 180;
   } else if (numParagraphs === 4) {
-    textLength = 400;
+    textLength = 240;
   } else {
-    textLength = 500;
+    textLength = 300;
   }
+  console.log(isChecked());
 
-  // Until the number of indecies is reached, using the random number funtion to grab a random index from the ballers array and pushing it to a new array
-  for (i = 0; i < textLength; i++) {
-    let randomIndex = getRandomNum(ballersArrayLength);
-    let wordToPush = loremIpsumDictionary[randomIndex];
-    tempArray.push(wordToPush);
+  // If user selected "Start with 'It's Football Time - Hey!'", insert this phrase in the zero index of the tempArray.
+  if (isChecked()) {
+    tempArray = [`It's Football Time - Hey!`];
+
+    // Until the number of indecies is reached, using the random number funtion to grab a random index from the ballers array and pushing it to a new array starting at the index of 1
+    for (i = 1; i < textLength; i++) {
+      let randomIndex = getRandomNum(ballersArrayLength);
+      let wordToPush = loremIpsumDictionary[randomIndex];
+      tempArray.push(wordToPush);
+    }
+  } else {
+    // Until the number of indecies is reached, using the random number funtion to grab a random index from the ballers array and pushing it to a new array starting at the index of 0
+    for (i = 0; i < textLength; i++) {
+      let randomIndex = getRandomNum(ballersArrayLength);
+      let wordToPush = loremIpsumDictionary[randomIndex];
+      tempArray.push(wordToPush);
+    }
   }
 
   // Adding line breaks based on length of returned array to represent paragraphs
-  if (tempArray.length === 200) {
-    tempArray.splice(100, 0, "<br><br>");
+  if (tempArray.length === 120) {
+    tempArray.splice(60, 0, "<br><br>");
+  } else if (tempArray.length === 180) {
+    tempArray.splice(60, 0, "<br><br>");
+    tempArray.splice(121, 0, "<br><br>");
+  } else if (tempArray.length === 240) {
+    tempArray.splice(60, 0, "<br><br>");
+    tempArray.splice(121, 0, "<br><br>");
+    tempArray.splice(182, 0, "<br><br>");
   } else if (tempArray.length === 300) {
-    tempArray.splice(100, 0, "<br><br>");
-    tempArray.splice(201, 0, "<br><br>");
-  } else if (tempArray.length === 400) {
-    tempArray.splice(100, 0, "<br><br>");
-    tempArray.splice(201, 0, "<br><br>");
-    tempArray.splice(302, 0, "<br><br>");
-  } else if (tempArray.length === 500) {
-    tempArray.splice(100, 0, "<br><br>");
-    tempArray.splice(201, 0, "<br><br>");
-    tempArray.splice(302, 0, "<br><br>");
-    tempArray.splice(403, 0, "<br><br>");
+    tempArray.splice(60, 0, "<br><br>");
+    tempArray.splice(121, 0, "<br><br>");
+    tempArray.splice(182, 0, "<br><br>");
+    tempArray.splice(243, 0, "<br><br>");
   }
 
-  // adding spaces to the array by creating a new object via the join method
+  // Adding spaces to the array by creating a new object via the join method
   let userIpsum = tempArray.join(" ");
 
-  // adding the text to the DOM and removing the hidden class to display the user's Fantasy Footballers Ipsum text
+  // Adding the text to the DOM and removing the hidden class to display the user's Fantasy Footballers Ipsum text
   result.innerHTML = userIpsum;
   result.classList.remove("hidden");
   e.preventDefault();
