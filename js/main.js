ca3db6c0ce4edc9ce1b11b97e01f3703beb596db
Dave-314land <dave.l.piland@gmail.com>
2021-06-01 21:52:52 -0400

    fixed issue where textLength variable wasn't being defined in getUserLoremIpsum. Added line breaks and more text based on paragraph value

--- js/main.js
+++ js/main.js
@@ -75,31 +75,55 @@ let loremIpsumDictionary = [
 
 // Define variables
 const form = document.querySelector(".lorem-form");
-const numberofParagraphsRange = document.getElementById(
-  "numberOfParagraphsRange"
-);
-const numberOfParagraphsNumber = document.getElementById(
-  "numberOfParagraphsNumber"
-);
-
 const result = document.querySelector(".lorem-text");
 const ballersArrayLength = loremIpsumDictionary.length;
-let text = 100;
+const paragraphsRange = document.getElementById("numberOfParagraphsRange");
+const paragraphsNumber = document.getElementById("numberOfParagraphsNumber");
 
 // Load event listeners
 loadEventListeners();
 
 function loadEventListeners() {
-  form.addEventListener("submit", getUserLoremIpsum);
-  numberOfParagraphsNumber.addEventListener("input", syncParagraphNumbers);
-  numberofParagraphsRange.addEventListener("input", syncParagraphNumbers);
+  form.addEventListener("submit", (e) => {
+    getUserLoremIpsum(e);
+  });
+  paragraphsNumber.addEventListener("input", syncParagraphNumbers);
+  paragraphsRange.addEventListener("input", syncParagraphNumbers);
 }
 
 function syncParagraphNumbers(e) {
   const value = e.target.value;
-  numberofParagraphsRange.value = value;
-  numberofParagraphsNumber.value = value;
+  paragraphsRange.value = value;
+  paragraphsNumber.value = value;
+  // let paragraphs = numberOfParagraphsNumber.value;
+
+  // return paragraphs;
 }
+/*
+function getNumParagraphs(paragraphs) {
+  //const value = e.target.value;
+  //numberOfParagraphsNumber.value = value;
+  let numberParagraphs = syncParagraphNumbers(paragraphs);
+  let numParagraphs = parseInt(numberParagraphs, 10);
+  if (numParagraphs === 1) {
+    textLength = 100;
+    console.log(`This is the text length value (should be 100): ${textLength}`);
+  } else if (numParagraphs === 2) {
+    textLength = 200;
+    console.log(`This is the text length value (should be 200): ${textLength}`);
+  } else if (numParagraphs === 3) {
+    textLength = 300;
+    console.log(`This is the text length value (should be 300): ${textLength}`);
+  } else if (numParagraphs === 4) {
+    textLength = 400;
+    console.log(`This is the text length value (should be 400): ${textLength}`);
+  } else {
+    textLength = 500;
+    console.log(`This is the text length value (should be 500): ${textLength}`);
+  }
+  return textLength;
+}
+*/
 
 function getRandomNum(ballersArrayLength) {
   return Math.floor(Math.random() * ballersArrayLength);
@@ -107,12 +131,52 @@ function getRandomNum(ballersArrayLength) {
 
 function getUserLoremIpsum(e) {
   let tempArray = [];
-  for (i = 0; i < text; i++) {
+  const value = paragraphsNumber.value;
+  let numParagraphs = parseInt(value, 10);
+  if (numParagraphs === 1) {
+    textLength = 100;
+    console.log(`This is the text length value (should be 100): ${textLength}`);
+  } else if (numParagraphs === 2) {
+    textLength = 200;
+    console.log(`This is the text length value (should be 200): ${textLength}`);
+  } else if (numParagraphs === 3) {
+    textLength = 300;
+    console.log(`This is the text length value (should be 300): ${textLength}`);
+  } else if (numParagraphs === 4) {
+    textLength = 400;
+    console.log(`This is the text length value (should be 400): ${textLength}`);
+  } else {
+    textLength = 500;
+    console.log(`This is the text length value (should be 500): ${textLength}`);
+  }
+  console.log(`This here be the text value: ${textLength}`);
+  console.log(typeof textLength);
+  for (i = 0; i < textLength; i++) {
     let randomIndex = getRandomNum(ballersArrayLength);
     let wordToPush = loremIpsumDictionary[randomIndex];
     tempArray.push(wordToPush);
   }
+
+  // Adding line breaks based on length of returned array to represent paragraphs
+  if (tempArray.length === 200) {
+    tempArray.splice(100, 0, "<br><br>");
+  } else if (tempArray.length === 300) {
+    tempArray.splice(100, 0, "<br><br>");
+    tempArray.splice(201, 0, "<br><br>");
+  } else if (tempArray.length === 400) {
+    tempArray.splice(100, 0, "<br><br>");
+    tempArray.splice(201, 0, "<br><br>");
+    tempArray.splice(302, 0, "<br><br>");
+  } else if (tempArray.length === 500) {
+    tempArray.splice(100, 0, "<br><br>");
+    tempArray.splice(201, 0, "<br><br>");
+    tempArray.splice(302, 0, "<br><br>");
+    tempArray.splice(403, 0, "<br><br>");
+  }
+
+  // Adding spaces between each index in the array
   let userIpsumArray = tempArray.join(" ");
+
   result.innerHTML = userIpsumArray;
   result.classList.remove("hidden");
   e.preventDefault();
